78a978c3ba653861c9f6ab1d1d277767b6db4a16
Roshan Diwakar <roshan.diwakar@gmail.com>
2017-04-16 15:27:44 -0400

    Enhanced Prime Number finding solution to stop at sqrt(n).
    Tested BigDecimal with Strings

--- java/src/euler/Euler.java
+++ java/src/euler/Euler.java
@@ -1,5 +1,6 @@
 package euler;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import util.Result;
 
@@ -7,22 +8,31 @@ import util.Result;
  * Created by rdiwakar on 4/16/2017.
  */
 public class Euler {
+
+    private Euler() {}; // Static functions
     public static Result getNthPrime(int n) {
         long startTime= System.nanoTime();
-        int primesFound= 1;
-        long currentPrime= 2;
+        int primesFound= 2;
+        long currentPrime= 3;
         Result[] results= new Result[n];
-        results[0]= new Result(currentPrime, System.nanoTime()-startTime);
+        results[0]= new Result(2, System.nanoTime()-startTime);
+        results[1]= new Result(currentPrime, System.nanoTime()-startTime);
 
         do {
             boolean nextPrimeFound= false;
+
             do {
                 int i=0;
                 currentPrime++;
-                while(i<primesFound) {
-                    if (currentPrime % results[i++].getResult() == 0) break;
+                boolean foundDivisor= false;
+                long checkUpto = (long)Math.floor(Math.sqrt(currentPrime));
+                while(results[i].getResult() <= checkUpto) {
+                    if (currentPrime % results[i++].getResult() == 0) {
+                        foundDivisor= true;
+                        break;
+                    }
                 }
-                if (i==primesFound) {
+                if (!foundDivisor) {
                     nextPrimeFound= true;
                     results[primesFound]= new Result(currentPrime, System.nanoTime()-startTime);
                     primesFound++;
@@ -32,15 +42,26 @@ public class Euler {
         } while (primesFound < n);
 
         for (int i=0; i<n; i++) {
-            //System.out.printf("%7d:%15d:%,15d\n",i, results[i].getResult(), results[i].getTimeToCompute());
+        //    System.out.printf("%7d:%15d:%,15d\n",i, results[i].getResult(), results[i].getTimeToCompute());
         }
         return results[n-1];
     }
 
-    public static void main(String[] args) {
+    public static long getLargeSum() {
+        BigDecimal l= new BigDecimal("37107287533902102798797998220837590246510135740250");
+        System.out.println(l.toString());
+        return 0L;
+    }
+
+    public static void solveNthPrime() {
         int n=10001;
         Result result= Euler.getNthPrime(n);
-        System.out.printf("%dth prime : %d, time:%,d\n",n, result.getResult(), result.getTimeToCompute());
+        System.out.printf("%dth prime : %d, time:%,d%n",n, result.getResult(), result.getTimeToCompute());
+    }
+
+    public static void main(String[] args) {
+        solveNthPrime();                          // 007
+        //getLargeSum();                              // 013
     }
 
 }
