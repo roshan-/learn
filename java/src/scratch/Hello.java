955f1e562fe698c344b5a477a1829c3fc8a62243
roshan.diwakar@gmail.com <roshan@australia.data.com>
2017-04-16 00:37:51 -0400

    Initial Structure for Tutorial Projects

---
+++ java/src/scratch/Hello.java
@@ -0,0 +1,10 @@
+package scratch;
+
+/**
+ * Created by rdiwakar on 4/15/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
